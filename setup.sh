95501be7a8dfb87573302fb9e7327dc015db44f0
eroc <erichadley8@gmail.com>
2021-01-29 12:35:31 -0500

    asdasdasda

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt install python3 -y
 apt install python3-pip -y
 pip3 install selenium
 apt install wget -y
-wget https://github./com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz
+wget https://www.github.com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz
 apt install tar -y
 tar -xf geckgeckodriver-v0.29.0-linux64.tar.gz
 python3 code.py
