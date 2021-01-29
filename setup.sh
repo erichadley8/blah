8500b9bd352ffb7ccc8fe995c26307faa690c395
eroc <erichadley8@gmail.com>
2021-01-29 12:11:45 -0500

    sadkasdjsadad

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
-sudo apt update
-sudo apt install firefox -y
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo pip3 install selenium
-sudo apt install wget -y
-sudo wget https://github./com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz
-sudo apt install tar -y
-sudo tar -xf geckgeckodriver-v0.29.0-linux64.tar.gz
-sudo apt install git -y
-sudo git clone https://www.github.com/erichadley8/blah.git
-sudo mv geckodriver /blah
-sudo cd /blah
-sudo python3 code.py
+apt update
+apt install firefox -y
+apt install python3 -y
+apt install python3-pip -y
+pip3 install selenium
+apt install wget -y
+wget https://github./com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz
+apt install tar -y
+tar -xf geckgeckodriver-v0.29.0-linux64.tar.gz
+python3 code.py
