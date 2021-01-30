4e3e2975f1c971fb78cf93aa9a19207f87c3e97a
root <root@ip-172-31-26-111.ec2.internal>
2021-01-30 23:30:48 +0000

    asdasdsaddsa

--- code.py
+++ code.py
@@ -7,21 +7,23 @@ while True:
 	executable_path = os.getcwd()+"/geckodriver"
 	options = Options()
 	options.headless = True
-	browser = webdriver.Firefox(options=options,executable_path=executable_path)
 	try:
-		browser.get("http://hdjshrn.tk/")
-		print(browser.page_source)
-		body = browser.find_elements_by_tag_name("body")
-		print(body)
-		body[0].click()
-		browser.switch_to.window(browser.window_handles[1])
-		ad = browser.find_elements_by_tag_name("iframe")
-		print(ad)
-		ad[0].click()
-		for tab in browser.window_handles:
-			browser.switch_to.window(tab)
-			browser.close()
+		browser = webdriver.Firefox(options=options,executable_path=executable_path)
+		try:
+			browser.get("http://hdjshrn.tk/")
+			print(browser.page_source)
+			body = browser.find_elements_by_tag_name("body")
+			print(body)
+			body[0].click()
+			browser.switch_to.window(browser.window_handles[1])
+			ad = browser.find_elements_by_tag_name("iframe")
+			print(ad)
+			ad[0].click()
+			for tab in browser.window_handles:
+				browser.switch_to.window(tab)
+				browser.close()
+		except Exception:
+			subprocess.Popen(["kill","$(pgrep)"])
 	except Exception:
-		subprocess.Popen(["kill","$(pgrep)"])
-
+		print('lazy')
 	#browser.quit()
