b7fb6c324b9bea6471751d1aac5f11e7b53721d6
eroc <erichadley8@gmail.com>
2021-01-31 11:08:44 -0500

    sadasdsaddsd

--- code.py
+++ code.py
@@ -3,24 +3,37 @@ import subprocess
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-while True:
-	executable_path = os.getcwd()+"/geckodriver"
-	options = Options()
-	options.headless = True
-	browser = webdriver.Firefox(options=options,executable_path=executable_path)
+executable_path = os.getcwd()+"/geckodriver"
+options = Options()
+options.headless = True
+
+def hit():
 	try:
+		#print("1")
+		browser = webdriver.Firefox(options=options,executable_path=executable_path)
+	except Exception:
+		print("Failed")
+		hit()
+	
+	try:
+		#print("2")
 		browser.get("http://hdjshrn.tk/")
 		print(browser.page_source)
 		body = browser.find_elements_by_tag_name("body")
 		print(body)
 		body[0].click()
+		
+		
 		browser.switch_to.window(browser.window_handles[1])
 		ad = browser.find_elements_by_tag_name("iframe")
 		print(ad)
 		ad[0].click()
+		
 		for tab in browser.window_handles:
 			browser.switch_to.window(tab)
 			browser.close()
 	except Exception:
-		subprocess.Popen(["kill","$(pgrep firefox)"])
-	#browser.quit()
+		subprocess.Popen(["kill","$(pgrep","firefox)"])
+
+while True:
+	hit()
