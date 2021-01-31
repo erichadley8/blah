ae4400fa7da83a7515925a79b17ba4575478c110
root <root@ip-172-31-28-147.ec2.internal>
2021-01-31 17:43:27 +0000

    asdasdssdass

--- code.py
+++ code.py
@@ -33,7 +33,7 @@ def hit():
 			browser.switch_to.window(tab)
 			browser.close()
 	except Exception:
-		subprocess.Popen(["sh","kill","end.sh"])
+		subprocess.Popen(["kill","end.sh"])
 
 while True:
 	hit()
